9d96293b6a9c82a577c2720afefb0131ed113ced
primetimenumberline <primetimenumberline@gmail.com>
2022-02-12 17:31:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int n = 101;
+for (int i = 2; i <= n; i++)
+{
+    if (isPrime(i))
+        Console.WriteLine(i);
+}
+bool isPrime(int i)
+{
+    for (int j = 2; j*j <= i; j++)
+    {
+        if ( mod(i,j) == 0 )
+            return false;
+    }
+    return true;
+}
+int mod(int i, int j)
+{
+    int q = 1;
+    int r = 0;
+    while ( q * j <= i)
+    {
+        q++;
+    }
+    r = i - ((q - 1) * j);
+    return r;
+}
